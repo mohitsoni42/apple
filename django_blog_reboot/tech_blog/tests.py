4ad93869aa75c16210dc59461a3aaffef39ccfeb
Mohit Soni <sonim@juniper.net>
2023-12-06 16:03:04 +0530

    added a test case

--- django_blog_reboot/tech_blog/tests.py
+++ django_blog_reboot/tech_blog/tests.py
@@ -1,4 +1,5 @@
 from django.test import TestCase
+from .models import ArticleModel
 
 
 # Create your tests here.
@@ -9,3 +10,9 @@ class HomeTest(TestCase):
         self.assertEqual(res.content ,b"heyyyy o")
 
         
+class ArticleTest(TestCase):
+    def test_article_created_success(self):
+        ArticleModel.objects.create(title="Test Title", author="Test Author", category="Test Category", content="Test Content", created_at=datetime.now(tz=timezone.utc))
+        article = ArticleModel.objects.get(title="Test Title")
+
+        self.assertEqual(article.category,"Test Category")
